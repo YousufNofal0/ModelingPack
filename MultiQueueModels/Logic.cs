1d0b664b44fe17e71e5440200eda45902ccc70ba
YousufNofal0 <YousufNofal2@gmail.com>
2023-10-27 11:13:00 +0200

    Adding logic functions

---
+++ MultiQueueModels/Logic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiQueueModels
+{
+    public static class Logic
+    {
+        public static void ReadFromFile()
+        {
+            //To Do: read the data from a file
+        }
+
+        public static void CalculateCommulativeAndRange(List<TimeDistribution> timeDistributionList)
+        {
+            //To Do: calculate the commulative probability and range for a give time distribution
+        }
+
+        public static void CalculateServerPerformanceMeasures(Server server)
+        {
+            //To Do: Calculate the performance measures for a given server 
+        }
+
+        public static void CalculateSystemPerformanceMeasures()
+        {
+            //To Do: Calculate the performance measures for the system 
+        }
+
+    }
+}
